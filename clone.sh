5c904030bd0cbe7bbc9eca7a5c6115ad50ef96ca
Luigi Cruz <alainluigicruz@gmail.com>
2021-02-22 23:15:49 +0800

    Add repo cloner

---
+++ clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Cloning repositories..."
+
+SITES=$HOME/Sites
+BLADE=$SITES/blade-ui-kit
+LARAVEL=$SITES/laravel
+
+# Personal
+git clone git@github.com:luigircruz/luigircruz.com.git $SITES/luigicruz.dev
+git clone git@github.com:luigircruz/og-image.git $SITES/og-image
+git clone git@github.com:luigircruz/wizard.git $SITES/wizard
+
+# Laravel
+git clone git@github.com:laravel/laravel.git $LARAVEL/laravel
