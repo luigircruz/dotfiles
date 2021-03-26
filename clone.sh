297bcbc5f5ca76ee35081b94e8c42b1805357887
Luigi Cruz <alainluigicruz@gmail.com>
2021-03-26 15:02:15 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,11 +4,10 @@ echo "Cloning repositories..."
 
 SITES=$HOME/Sites
 LARAVEL=$SITES/laravel
+PKGS=$HOME/Packages
 
 # Personal
 git clone git@github.com:luigircruz/luigircruz.com.git $SITES/luigicruz.dev
 git clone git@github.com:luigircruz/og-image.git $SITES/og-image
 git clone git@github.com:luigircruz/wizard.git $SITES/wizard
-
-# Laravel
-git clone git@github.com:laravel/laravel.git $LARAVEL/laravel
+git clone git@github.com:luigircruz/ui-fragments.git $PKGS/ui-fragments
