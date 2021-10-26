6d5c20bfdc8b29db850399fe0459720862d20fae
Luigi <luigicruz.dev@gmail.com>
2021-10-26 18:41:12 +0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -2,9 +2,14 @@
 
 echo "Setting up your Mac..."
 
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
 # Update Homebrew recipes
@@ -14,11 +19,11 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
+# Set default MySQL root password and auth type
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
-pecl install memcached imagick
+pecl install imagick memcached redis swoole
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose
@@ -27,11 +32,9 @@ pecl install memcached imagick
 $HOME/.composer/vendor/bin/valet install
 
 # Create a Sites directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/Sites
 
 # Create sites subdirectories
-# mkdir $HOME/Sites/blade-ui-kit
 mkdir $HOME/Sites/laravel
 
 # Clone Github repositories
@@ -44,6 +47,5 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
+# Set macOS preferences - we will run this last because this will reload the shell
 source .macos
