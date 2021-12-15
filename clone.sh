4948e4d5b6c513c185247f04763584af6414d0e6
Luigi <luigicruz.dev@gmail.com>
2021-12-15 23:16:22 +0800

    Update dotfiles

--- clone.sh
+++ clone.sh
@@ -8,6 +8,3 @@ PKGS=$HOME/Packages
 
 # Personal
 git clone git@github.com:luigircruz/luigicruz.dev.git $SITES/luigicruz.dev
-git clone git@github.com:luigircruz/og-image.git $SITES/og-image
-git clone git@github.com:luigircruz/wizard.git $SITES/wizard
-git clone git@github.com:luigircruz/ui-fragments.git $PKGS/ui-fragments
