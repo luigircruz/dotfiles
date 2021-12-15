0aceaf6a7a914fcea1a8a146bf47dc8d541857b5
Luigi <luigicruz.dev@gmail.com>
2021-12-15 23:21:31 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,7 +4,6 @@ echo "Cloning repositories..."
 
 SITES=$HOME/Sites
 LARAVEL=$SITES/laravel
-PKGS=$HOME/Packages
 
 # Personal
 git clone git@github.com:luigircruz/luigicruz.dev.git $SITES/luigicruz.dev
