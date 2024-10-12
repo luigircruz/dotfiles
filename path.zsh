d3fad1170acfe64e62286a47819e3c2cfbb18e77
Luigi <luigicruz.dev@gmail.com>
2024-10-12 11:33:54 +0800

    Update path.zsh

--- path.zsh
+++ path.zsh
@@ -1,14 +1,22 @@
+# Add directories to the PATH and prevent to add the same directory multiple times upon shell reload.
+add_to_path() {
+  if [[ -d "$1" ]] && [[ ":$PATH:" != *":$1:"* ]]; then
+    export PATH="$1:$PATH"
+  fi
+}
+
 # Load dotfiles binaries
-export PATH="$DOTFILES/bin:$PATH"
+add_to_path "$DOTFILES/bin"
 
 # Load Composer tools
-export PATH="$HOME/.composer/vendor/bin:$PATH"
+add_to_path "$HOME/.composer/vendor/bin"
 
 # Load Node global installed binaries
-export PATH="$HOME/.node/bin:$PATH"
+add_to_path "$HOME/.node/bin"
 
 # Use project specific binaries before global ones
-export PATH="node_modules/.bin:vendor/bin:$PATH"
+add_to_path "vendor/bin"
+add_to_path "node_modules/.bin"
 
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
