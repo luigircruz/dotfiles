025b1561b223bc1b04c99aaab6ac7059fbaa744e
Luigi Cruz <luigicruz.dev@gmail.com>
2025-10-19 19:29:45 +0800

    wip

--- config/nvim/mappings.lua
+++ config/nvim/mappings.lua
@@ -2,6 +2,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.number = true
 
+-- Tabs vs spaces
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
 local map = vim.keymap.set
 
 map("i", "<C-b>", "<ESC>^i", { desc = "Move to the beginning of line" })
@@ -27,18 +33,12 @@ map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
 map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
 map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
-map("n", "<leader>wl",
-  function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end,
-  { desc = "List workspace folders" }
-)
-map("n", "<leader>f",
-  function()
-    vim.lsp.buf.format({ async = true })
-  end,
-  { desc = "Format buffer" }
-)
+map("n", "<leader>wl", function()
+	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, { desc = "List workspace folders" })
+map("n", "<leader>f", function()
+	vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer" })
 
 -- File Tree
 map("n", "<leader>e", "<cmd>Neotree<CR>")
