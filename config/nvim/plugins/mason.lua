1a22cb15f95bd2602dcba47d5dd1c308f0e4eb63
Luigi Cruz <luigicruz.dev@gmail.com>
2025-10-21 19:20:21 +0800

    chores: fix errors

--- config/nvim/plugins/mason.lua
+++ config/nvim/plugins/mason.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-      },
-    },
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-      },
-    },
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    }
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-    },
-    config = function()
-      vim.lsp.enable({
-        "lua_ls",
-        "ts_ls",
-      })
-    end
-  }
-},
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"stylua",
+			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+			},
+		},
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"mason-org/mason-lspconfig.nvim",
+		},
+		config = function()
+			vim.lsp.enable({
+				"lua_ls",
+				"ts_ls",
+			})
+		end,
+	},
+}
